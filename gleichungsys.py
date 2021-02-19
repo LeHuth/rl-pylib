1421f748967d641b342506f492dd3a7e6498d9cc
Leo <s80387@beuth-hochschule.de>
2021-02-19 20:36:38 +0100

    discoverd mistake in gaussMethod()

--- gleichungsys.py
+++ gleichungsys.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def getData():
-    return "1.49,1.98;-0.48,-1.49;0.98,0.66;2.73,0.19"#input("Punkte angeben mit format: zahl.dezimal,zahl.dezimal;...")
+    return "-1.49,1.98;-0.48,-1.49;0.98,0.66;2.73,0.19"#input("Punkte angeben mit format: zahl.dezimal,zahl.dezimal;...")
 
 def splitDataIntoPoints(data):
     return data.split(";")
@@ -26,27 +26,23 @@ def createGleichungssystem(points):
         arr.clear()
     return np.array(bigger_arr)
 
-def gaussMethod(gl_sys, exponent):
+def gaussMethod(gls, exponent):
+    #rework as such: http://elsenaju.info/Rechner/Gleichungssystem-NxN.htm
     for meta in range(1,exponent+1):
         for i in range(meta,exponent+1):
-            multiplier = gl_sys[meta-1]/gl_sys[i]
-            res = gl_sys[i]*multiplier[meta-1] - gl_sys[meta-1]
-            gl_sys[i] = res
-
+            multiplier = gls[meta-1]/gls[i]
+            res = gls[i]*multiplier[meta-1] - gls[meta-1]
+            gls[i] = res
+    #clean up gls
     for j in range(exponent):  
         for i in range(j+1,exponent+1):
-            gl_sys[i][j] = 0
-            
-    return gl_sys
+            gls[i][j] = 0
+
+    return gls
 
 data = getData()  
 points = splitDataIntoPoints(data)
 gleichungssystem = createGleichungssystem(points)
+print(gleichungssystem)
 new_sys = gaussMethod(gleichungssystem, 3)
-#first step, first row
-
-#print(gleichungssystem)
-print(new_sys)
-for i in range(1,4):
-    print(i)
 
