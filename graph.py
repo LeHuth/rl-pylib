4fdc4c946efdb46d2127c383076cb924a1336007
Leo <s80387@beuth-hochschule.de>
2021-02-25 17:15:40 +0100

    changed constructor

--- graph.py
+++ graph.py
@@ -1,8 +1,6 @@
 import numpy as np
 class graph:
-    _id = -1
-    _points = np.array()
 
     def __init__(self, points, id):
-        self._points = points
+        self._points = np.array(points)
         self._id = id
