48c22b459ca7c38a05c196e2c806837cc7f9846f
Leo <s80387@beuth-hochschule.de>
2021-03-01 15:06:56 +0100

    plottet function now displays for every x

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,7 +1,6 @@
 #https://matplotlib.org/3.3.3/users/event_handling.html
 import matplotlib.pyplot as plt
 import numpy as np
-from NxN_gls_solver import gleichungsys
 
 def setup():
     global ax, fig, x
@@ -26,21 +25,22 @@ def setup():
 
 def onclick(event):
     print("click")
-    print(event.xdata)
-    print(event.ydata)
+    #print(event.xdata)
+    #print(event.ydata)
 
     plt.ion()
 
     global ax
-    ax.plot(event.xdata, event.ydata, 'ro')
+    
     if(len(points)<5):
+        ax.plot(event.xdata, event.ydata, 'ro')
         points.append(event.xdata)
         points.append(event.ydata)
 
 
-    if(len(points)%2==0 and len(points)!=0):
+    if(len(points)==4 and len(points)!=0):
         try:
-            global x
+            global x, te,b,slope
             slope = (points[3] - points[1]) / (points[2] - points[0])
 
             temp = slope * points[0]
@@ -49,26 +49,41 @@ def onclick(event):
             xd = np.linspace(-10,10,100)
 
             te = slope * x + b
-            plt.plot(xd,te, '-r')
+            plt.plot(x,te, '-r')
 
-            print("googgo")
-            points.clear()
+            print(te)
+            #points.clear()
         except:
             pass
 
     plt.ioff()
 
+def onrelease(event):
+    global ax, x, slope,b
+    x1,x2 = ax.get_xlim()
+    print(ax.get_xlim())
+    print(ax.get_ylim())
+    x = np.arange(x1,x2,0.2)
+    func = slope * x + b
+    print(te)
+    plt.plot(x,func, '-r')
+
 def start():
     global cid, plt
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    cid = fig.canvas.mpl_connect('button_release_event', onrelease)
     plt.plot()
     plt.show()
 
 points = []
-fig = 0;
-ax = 0;
-x = 0;
-
+fig = 0
+ax = 0
+x = 0
+te = 0
+slope = 0
+b = 0
+setup()
+start()
 #for every new graph instatiate new graph object. 
 #save that object in some datastructure with a unique id.
 #write someting that keeps this data structre sorted and withouts empty positions
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hi")
+
+from graph import graph
+g =  graph([1,2,3],1)
+print(g._points)
